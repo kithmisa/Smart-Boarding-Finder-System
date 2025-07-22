7b7f3a0b20a78d711d10a71228625403276e4940
ChamodiPerera0503 <chamodiperera000503@gmail.com>
2025-07-22 16:54:33 +0530

    Search part is completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
 import Search from './components/Search';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import About from './components/About';
 
 import './App.css';
 
 
 function App() {
   return (
-      <><Routes>
+     <><Routes>
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
+         <Route path="/about" element={<About />} />
 
 
     </Routes><div className="bg-gray-100">
         <Navbar />
         <Footer />
+         <ScrollToTop />
       </div></>
+
+
+
   );
 }
 
-export default App
+export default App;
