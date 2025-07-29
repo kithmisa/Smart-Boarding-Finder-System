9c36a1fca5678e127a685c8779b9f4baa2353a44
Sithumina De Sliva <ysithu221@gmail.com>
2025-07-29 10:51:56 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,10 +89,7 @@ const Footer = () => {
 
       </div>
 
-       {/* Copyright Section */}
-      <div className="text-center text-sm mt-6 border-t border-gray-500 pt-4">
-        © 2025 Smart Boarding Finder. All rights reserved.
-      </div>
+     
     </footer>
   );
 };
